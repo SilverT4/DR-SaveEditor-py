4c9d8295b3e44c86bc172917913c929283766d65
Silver T. <silvertn4@gmail.com>
2025-06-08 10:58:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deltarune Save Editor
+
+This is meant to be a Python-based save editor for Deltarune, with support for chapters 1-4.
---
+++ __init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+__doc__ = """A Python based save editor for DELTARUNE. Supports chapters 1-4.
+
+Usage: TBA"""
+__author__ = "SilverT4"
